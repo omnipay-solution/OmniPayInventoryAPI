23a881f2f4b7031e1c80116b92a47c5fb7ed1cc9
AshishD0d <ashish.dod@omnipay-solution.com>
2025-06-28 14:36:34 +0530

    Update index.js

--- OmniPayInventorymanagementAPI/index.js
+++ OmniPayInventorymanagementAPI/index.js
@@ -8,25 +8,18 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// ✅ Middleware
 app.use(cors());
-app.use(express.json()); // replaces body-parser
-
-
-app.use(bodyParser.json()); // for parsing application/json
+app.use(express.json());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// ✅ Routes
 app.use('/api', authRoutes);
 app.use('/api', productRoutes);
 
-
-// ✅ Root route
 app.get('/', (req, res) => {
   res.send('Welcome to OmniPayInventorymanagementAPI!');
 });
 
-// ✅ Start server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
