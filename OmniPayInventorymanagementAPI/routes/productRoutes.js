edbe82f0a900f19370f58d2eb3b65c6fe7bf2cd0
AshishD0d <ashish.dod@omnipay-solution.com>
2025-07-28 22:22:17 +0530

    Update productRoutes.js

--- OmniPayInventorymanagementAPI/routes/productRoutes.js
+++ OmniPayInventorymanagementAPI/routes/productRoutes.js
@@ -1,14 +1,45 @@
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { getAllProducts, getProductByUPC , getAllCategories, getActiveSalesTax, getCreditCardChargeConfig, createProduct} = require('../controllers/productController');
+const {
+  getAllProducts,
+  getProductByUPC,
+  getAllCategories,
+  getActiveSalesTax,
+  getCreditCardChargeConfig,
+  createProduct,
+  getInventoryTrackingData,
+  getFlashReport,
+  getCategoryById,
+  getSalesHistory,
+  getHourlyReport
+} = require("../controllers/productController");
 
 // GET /products
-router.get('/products', getAllProducts);
-router.get('/products/:upc_code', getProductByUPC);
-router.get('/categories', getAllCategories);
-router.get('/sales-tax', getActiveSalesTax);
-router.get('/config/creditcardcharge', getCreditCardChargeConfig);
-router.post('/products', createProduct);
+// static routes
+router.get("/categories", getAllCategories);
+router.get("/sales-tax", getActiveSalesTax);
+router.get("/config/creditcardcharge", getCreditCardChargeConfig);
+router.post("/inventory-tracking", getInventoryTrackingData);
+router.post("/flash-report", getFlashReport);
+router.post("/category", getCategoryById);
+router.post("/sales-history", getSalesHistory);
+router.post("/hourly-report", getHourlyReport);
+
+// dynamic routes
+router.get("/", getAllProducts);
+router.post("/", createProduct);
+router.get("/:upc_code", async (req, res, next) => {
+  const { upc_code } = req.params;
+
+  //  Only proceed if it's a valid UPC format
+  if (!/^\d{5,15}$/.test(upc_code)) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Invalid UPC format" });
+  }
+
+  // Call actual controller
+  return getProductByUPC(req, res, next);
+});
 
 module.exports = router;
