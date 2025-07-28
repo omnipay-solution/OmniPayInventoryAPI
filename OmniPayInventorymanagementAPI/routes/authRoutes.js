5e74df92d565b2dbce25f8a9d73a218fa7df5e20
AshishD0d <ashish.dod@omnipay-solution.com>
2025-07-28 22:20:54 +0530

    Update authRoutes.js

--- OmniPayInventorymanagementAPI/routes/authRoutes.js
+++ OmniPayInventorymanagementAPI/routes/authRoutes.js
@@ -1,8 +1,9 @@
 // routes/authRoutes.js
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/authController');
+const { login } = require('../controllers/authController');
 
-router.post('/login', authController.login);
+// Login route
+router.post('/login', login);
 
 module.exports = router;
