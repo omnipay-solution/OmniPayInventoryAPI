c7cdf3071000ef24e0f2ceff49c70ca50e7900c8
AshishD0d <ashish.dod@omnipay-solution.com>
2025-07-31 22:23:52 +0530

    Update productRoutes.js

--- OmniPayInventorymanagementAPI/routes/productRoutes.js
+++ OmniPayInventorymanagementAPI/routes/productRoutes.js
@@ -11,9 +11,10 @@ const {
   getFlashReport,
   getCategoryById,
   getSalesHistory,
-  getHourlyReport
+  getHourlyItemReport,
+  getUserCoins,
 } = require("../controllers/productController");
-
+ 
 // GET /products
 // static routes
 router.get("/categories", getAllCategories);
@@ -23,23 +24,26 @@ router.post("/inventory-tracking", getInventoryTrackingData);
 router.post("/flash-report", getFlashReport);
 router.post("/category", getCategoryById);
 router.post("/sales-history", getSalesHistory);
-router.post("/hourly-report", getHourlyReport);
-
+router.post("/hourly-report", getHourlyItemReport);
+router.post("/userCoins", getUserCoins);
+ 
 // dynamic routes
 router.get("/", getAllProducts);
 router.post("/", createProduct);
 router.get("/:upc_code", async (req, res, next) => {
   const { upc_code } = req.params;
-
+ 
   //  Only proceed if it's a valid UPC format
   if (!/^\d{5,15}$/.test(upc_code)) {
     return res
       .status(400)
       .json({ success: false, message: "Invalid UPC format" });
   }
-
+ 
   // Call actual controller
   return getProductByUPC(req, res, next);
 });
-
+ 
 module.exports = router;
+ 
+ 
