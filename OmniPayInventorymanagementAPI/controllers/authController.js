cfa341bb3b46ae5e625a16c677c818c1202f655d
AshishD0d <ashish.dod@omnipay-solution.com>
2025-08-23 02:27:05 +0530

    Update authController.js

--- OmniPayInventorymanagementAPI/controllers/authController.js
+++ OmniPayInventorymanagementAPI/controllers/authController.js
@@ -18,7 +18,7 @@ const login = async (req, res) => {
       return res.status(401).json({ message: 'Invalid username or password' });
     }
 
-    res.status(500).json({
+    res.status(200).json({
       message: 'Login successful',
       user: {
         LUserCode: user.UserCode,
@@ -37,5 +37,4 @@ const login = async (req, res) => {
 module.exports = {
   login
 };
-
 
